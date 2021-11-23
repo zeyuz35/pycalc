5269769e27a03abc71d9575167ce3957beee8b95
Zeyu Zhong <zeyu123@gmail.com>
2021-11-23 11:24:49 +1100

    support for multiplication

--- calc.py
+++ calc.py
@@ -4,9 +4,12 @@ def compute(expression):
         return num0 + num1
     elif operator == '-':
         return num0 - num1
+    elif operator == '*':
+        return num0 * num1
     else:
         print('unknown operator!')
         return None
+        
 
 # more docing
 
