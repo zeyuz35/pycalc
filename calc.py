b8bd396c6c6d6dc7fccf65732349b9a7488152da
Zeyu Zhong <zeyu123@gmail.com>
2021-11-23 11:08:20 +1100

    new branch test

--- calc.py
+++ calc.py
@@ -1,13 +1,13 @@
 def compute(expression):
-    values = expression.split(' ')
-    num0 = int(values[0])
-    operator = values[1]
-    num1 = int(values[2])
+    num0, operator, num1 = expression.split(' ')
     if operator == '+':
         return num0 + num1
+    elif operator == '-':
+        return num0 - num1
     else:
         print('unknown operator!')
         return None
+
 # Test
 
 # print(compute("32 + 32"))
